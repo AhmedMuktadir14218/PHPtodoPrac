75f719498ee491f15e91892f75768d98c620f06d
AhmedMuktadirUdoy <100287308+AhmedMuktadir14218@users.noreply.github.com>
2023-04-02 12:08:37 +0600

    Update code.php

--- code.php
+++ code.php
@@ -5,16 +5,26 @@ $name = $_POST|'email'|
 
 echo $name;
 
+
 $host = 'localhost';
 $user = 'root';
 $password = '';
 $db = 'test';
+$con = mysqli_connect($host, $user, $password, $db);
 
-$con = mysqli_connect($host,$user,$password,$db);
-
-if (!$con) {
-    die("connection failed : " .mysqli_connect_error());
+if(!$con){
+    die("Connection failed:" .mysqli_connect_error());
 }else{
-    echo "connection Succesfull"
+    echo "connection successful!!!";
 }
+
+
+$query = "INSERT INTO april(name, mobile,email) VALUES('$name', '$mobile', '$email')";
+$run = mysqli_query($con, $query);
+
+if(!run){
+    echo "submission failed";
+}
+
+
 ?>
